0f9116c14dbeeab18482523ee8c6bf087325b50b
emerson pereira da silva <166187148+emerson-silva91@users.noreply.github.com>
2024-05-23 22:11:47 -0300

    Add files via upload

--- Atividades/atividade6/script.js
+++ Atividades/atividade6/script.js
@@ -1,35 +1,34 @@
-let valor1 = document.querySelector ("#valor1");
-let valor2 = document.querySelector ("#valor2");
-let valor3 = document.querySelector ("#valor3");
-let valor4 = document.querySelector ("#valor4");
-let verifMenor = document.querySelector ("#verifMenor");
-let h1menor = document.querySelector ("#h1menor");
+let valor1 = document.querySelector("#valor1");
+let valor2 = document.querySelector("#valor2");
+let valor3 = document.querySelector("#valor3");
+let valor4 = document.querySelector("#valor4");
+let verifMenor = document.querySelector("#verifMenor");
+let h1menor = document.querySelector("#h1menor");
 
-function verificarmenor (){
+function verificarmenor() {
     let comp1 = Number(valor1.value);
     let comp2 = Number(valor2.value);
     let comp3 = Number(valor3.value);
     let comp4 = Number(valor4.value);
 
-   if(comp1<=comp2){
-    h1menor (comp1)
-   }
-
+    if (comp1 < comp2 && comp1 < comp3 && comp1 < comp4) {
+        h1menor.textContent = `O menor valor  ${comp1}`;
+    } else if (comp2 < comp1 && comp2 < comp3 && comp2 < comp4) {
+        h1menor.textContent = `O menor valor ${comp2}`;
+    } else if (comp3 < comp1 && comp3 < comp2 && comp3 < comp4) {
+        h1menor.textContent = `O menor valor  ${comp3}`;
+    } else if (comp4 < comp1 && comp4 < comp2 && comp4 < comp3) {
+        h1menor.textContent = `O menor valor  ${comp4}`;
+    } else {
+        h1menor.textContent = "Todos os valores são iguais.";
+    }
 
 
 }
 
 
 
-
-
-
-
-
-
-
-
-verifMenor.onclick = function (){
+verifMenor.onclick = function () {
 
     verificarmenor();
 
